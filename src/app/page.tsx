aa762f91909ab83d162a10dfd4a3a32d85ec9701
Aman Arora <info@makeitlook.co.uk>
2025-07-29 13:29:16 +0100

    clean up page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import AboutPreview from "@/components/AboutPreview/AboutPreview";
-import AnimatedDiv from "@/components/AnimatedDiv/AnimatedDiv";
 import GalleryPreview from "@/components/GalleryPreview/GalleryPreview";
 import HeroCarousel from "@/components/HeroSection/HeroSection";
-import PageLayout from "@/components/Layouts/PageLayout";
 import ServicesOverview from "@/components/ServicesOverview/ServicesOverview";
 import TestimonialsSection from "@/components/TestimonialsSection/TestimonialsSection";
 
